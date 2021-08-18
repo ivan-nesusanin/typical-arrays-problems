dffdd7b97fdb04786e1f66da49763c5cb8c34c49
ivan-nesusanin <l75x5@yandex.ru>
2021-08-18 17:29:55 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let min = array[0];
+    let i = 1;
+    while (i < array.length) {
+      if (min < array[i]) {
+        i++;
+      } else {
+        min = array[i];
+        i++;
+      }
+    }
+    return min;
+  };
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let max = array[0];
+    let i = 1;
+    while (i < array.length) {
+      if (max > array[i]) {
+        i++;
+      } else {
+        max = array[i];
+        i++;
+      }
+    }
+    return max;
+  };
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    const average = sum / array.length;
+    return average;
+  };
